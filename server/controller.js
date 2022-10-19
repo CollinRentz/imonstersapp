4cbf6fef6366177ce2e9b7596cde3f6ae913987e
Collin Rentz <rentzcollin@gmail.com>
2022-10-19 15:04:36 -0600

    .

--- server/controller.js
+++ server/controller.js
@@ -2,6 +2,7 @@ const path = require('path')
 const monsters = require('./db.json')
 let globalId = 4
 module.exports = {
+    
     getHTML: (req, res) => {
         res.sendFile(path.join(__dirname, '../public/index.html'));
     },
