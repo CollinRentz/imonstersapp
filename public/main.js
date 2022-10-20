4342f696588b132eb487911b1c87c8e6aa5eef52
Collin Rentz <rentzcollin@gmail.com>
2022-10-20 13:33:21 -0600

    .

--- public/main.js
+++ public/main.js
@@ -58,3 +58,12 @@ function displayMonsters(arr) {
 form.addEventListener('submit', submitHandler)
 
 getAllMonsters()
+
+try {
+    nonExistentFunction();
+  } catch (error) {
+    console.error(error);
+    // expected output: ReferenceError: nonExistentFunction is not defined
+    // Note - error messages will vary depending on browser
+  }
+  
